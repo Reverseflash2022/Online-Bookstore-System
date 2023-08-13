525598bcb9c089126289f32c9be6389ed4545811
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:28:14 +0200

    Update models.py

--- app/reviews/models.py
+++ app/reviews/models.py
@@ -1,1 +1,11 @@
+from django.db import models
+
+class Review(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    book = models.ForeignKey('books.Book', on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField()
+    comment = models.TextField()
+
+    def __str__(self):
+        return f'Review by {self.user.username} for {self.book.title}'
 
