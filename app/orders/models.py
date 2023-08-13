919d9d72ca95e2680af80e6b42cd8cdb0ee593c3
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:27:40 +0200

    Update models.py

--- app/orders/models.py
+++ app/orders/models.py
@@ -1,1 +1,16 @@
+from django.db import models
+from books.models import Book
+
+class Order(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, default='PENDING')
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.book.title} - {self.quantity}'
 
