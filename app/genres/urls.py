6daf2495ead176e918f6e8f86e74aa16874586f3
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:35:50 +0200

    Update urls.py

--- app/genres/urls.py
+++ app/genres/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('', views.GenreListCreateView.as_view(), name='genre-list-create'),
+    path('<int:pk>/', views.GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail'),
+]
