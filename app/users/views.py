c3fcbb3baa246cd12c25e57509eb065aac63a1b0
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:33:52 +0200

    Create views.py

---
+++ app/users/views.py
@@ -0,0 +1,11 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from .serializers import UserSerializer
+
+class UserListView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserRetrieveView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
