140ac3a998969e35b2f4ed867681760cac17cbc5
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 18:50:04 +0200

    Create urls.py

---
+++ app/backend/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import BookListCreateView, BookRetrieveUpdateDestroyView
+
+urlpatterns = [
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyView.as_view(), name='book-retrieve-update-destroy'),
+    # Similar URL patterns for Author, Genre, Order, Review
+]
