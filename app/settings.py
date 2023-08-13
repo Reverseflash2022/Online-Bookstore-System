5c0085e51aadde8cf68317b884f32f140457f3a5
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:24:24 +0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -1,1 +1,21 @@
+INSTALLED_APPS = [
+    # ... other apps
+    'books',
+    'authors',
+    'genres',
+    'users',
+    'orders',
+    'reviews',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bookstore_db',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
 
