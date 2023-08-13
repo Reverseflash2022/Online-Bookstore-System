735de191723c333c423d3e5443e69fd2f81cc9bf
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:27:15 +0200

    Update models.py

--- app/users/models.py
+++ app/users/models.py
@@ -1,1 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('CUSTOMER', 'Customer'),
+        ('ADMIN', 'Admin'),
+    )
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='CUSTOMER')
+
+    def __str__(self):
+        return self.username
 
