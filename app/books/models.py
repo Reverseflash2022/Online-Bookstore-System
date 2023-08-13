b88a0a07c589e4869d789012c482a2218486b60e
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:25:05 +0200

    Update models.py

--- app/books/models.py
+++ app/books/models.py
@@ -1,1 +1,12 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey('authors.Author', on_delete=models.CASCADE)
+    genre = models.ForeignKey('genres.Genre', on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.title
 
