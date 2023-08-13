373d0db952cb601db63be63626309e0649bc7253
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:33:21 +0200

    Update views.py

--- app/genres/views.py
+++ app/genres/views.py
@@ -1,1 +1,11 @@
+from rest_framework import generics
+from .models import Genre
+from .serializers import GenreSerializer
 
+class GenreListCreateView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
