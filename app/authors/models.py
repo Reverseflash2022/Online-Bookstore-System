ebb643f86605b1af6bb614f8f282ae57ee772457
Reverseflash <105869679+Reverseflash2022@users.noreply.github.com>
2023-08-13 19:25:59 +0200

    Update models.py

--- app/authors/models.py
+++ app/authors/models.py
@@ -1,1 +1,9 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+    biography = models.TextField()
+
+    def __str__(self):
+        return self.name
 
